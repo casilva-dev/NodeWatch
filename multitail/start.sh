c52c6b15cb8f12b6f279edde7d1d8f5423e8646d
CaesarCoder <caesrcd@tutamail.com>
2026-02-03 02:48:24 -0300

    multitail: replace GMT with UTC in timezone format
    Updates regex and timestamp conversion to use UTC instead of GMT,
    following the ISO 8601 standard.

--- multitail/start.sh
+++ multitail/start.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 tail -n 20 -f "$1" | while IFS= read -r line; do
     timestamps=$(echo "$line" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z')
     for timestamp in $timestamps; do
-        local_time=$(date -d "${timestamp%Z} UTC" '+%Y-%m-%dT%H:%M:%SGMT%:::z')
+        local_time=$(date -d "${timestamp%Z} UTC" '+%Y-%m-%dT%H:%M:%SUTC%:::z')
         line=$(printf '%s\n' "$line" | sed "s~$timestamp~$local_time~g")
     done
     echo "$line"
